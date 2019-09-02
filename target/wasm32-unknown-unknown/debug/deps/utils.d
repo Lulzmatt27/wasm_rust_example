252cc38473606b9225cb0563a01e156581469cc7
lulz <lulmatt27@gmail.com>
2019-09-02 21:23:24 +0500

    examle wasm

---
+++ target/wasm32-unknown-unknown/debug/deps/utils.d
@@ -0,0 +1,5 @@
+/Users/lulz/Desktop/rust/webassembly/utils/target/wasm32-unknown-unknown/debug/deps/utils.wasm: src/lib.rs
+
+/Users/lulz/Desktop/rust/webassembly/utils/target/wasm32-unknown-unknown/debug/deps/utils.d: src/lib.rs
+
+src/lib.rs:
